7c6f745fb1eb6442d216f4c070a42fdd445dba36
Kevin Gomes <kevinmg50@gmail.com>
2023-08-09 19:40:59 -0300

    style: melhorado identação do código

--- src/services/ProductService.cs
+++ src/services/ProductService.cs
@@ -27,14 +27,23 @@ namespace StoreOps.Services
             return _products.Find(product => true).ToList();
         }
 
- public List<Product> SearchProducts(string search, ObjectId? categoryId = null)
-{
-    return _products.AsQueryable()
-        .Where(product =>
-            (string.IsNullOrEmpty(search) || product.Name.Contains(search) || product.Description.Contains(search)) &&
-            (!categoryId.HasValue || product.CategoryId == categoryId.Value.ToString()))
-        .ToList();
-}
-
+        public List<Product> SearchProducts(string search, ObjectId? categoryId = null)
+        {
+            return _products
+                .AsQueryable()
+                .Where(
+                    product =>
+                        (
+                            string.IsNullOrEmpty(search)
+                            || product.Name.Contains(search)
+                            || product.Description.Contains(search)
+                        )
+                        && (
+                            !categoryId.HasValue
+                            || product.CategoryId == categoryId.Value.ToString()
+                        )
+                )
+                .ToList();
+        }
     }
 }
