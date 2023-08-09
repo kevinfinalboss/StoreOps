3a5390b48893d4f563fa1a0595e535fd34ca25e9
Kevin Gomes <kevinmg50@gmail.com>
2023-08-09 09:02:55 -0300

    fix: removido a importação do system

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using StoreOps.Database;
+﻿using StoreOps.Database;
 
 namespace StoreOps
 {
@@ -12,7 +11,7 @@ namespace StoreOps
 
             Menu menu = new();
 
-            menu.ShowMenu();
+            Menu.ShowMenu();
         }
     }
 }
