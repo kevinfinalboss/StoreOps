4698f1a9dd15e1d6ddbdc5243483fff5565ab72b
Kevin Gomes <kevinmg50@gmail.com>
2023-08-09 18:42:59 -0300

    feat: adicionado regex para CPF e telefone

--- src/services/CustomerService.cs
+++ src/services/CustomerService.cs
@@ -1,13 +1,12 @@
 using MongoDB.Driver;
 using StoreOps.Database;
 using MongoDB.Bson;
-
 using StoreOps.Models;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace StoreOps.Services
 {
-    public class CustomerService
+    public partial class CustomerService
     {
         private readonly IMongoCollection<Customer> _customers;
 
@@ -18,6 +17,11 @@ namespace StoreOps.Services
 
         public Customer AddCustomer(Customer customer)
         {
+            if (!IsValidEmail(customer.Email)) throw new FormatException("Email inválido");
+
+            customer.CPF = FormatCPF(customer.CPF);
+            customer.PhoneNumber = FormatPhoneNumber(customer.PhoneNumber);
+
             _customers.InsertOne(customer);
             return customer;
         }
@@ -31,5 +35,33 @@ namespace StoreOps.Services
         {
             _customers.DeleteOne(customer => customer.Id == id);
         }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var regexPattern = @"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$";
+                return Regex.IsMatch(email, regexPattern);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string FormatCPF(string cpf)
+        {
+            return MyRegex().Replace(cpf, "$1.$2.$3-$4");
+        }
+
+        private string FormatPhoneNumber(string phoneNumber)
+        {
+            return MyRegex1().Replace(phoneNumber, "($1) $2-$3");
+        }
+
+        [GeneratedRegex("(\\d{3})(\\d{3})(\\d{3})(\\d{2})")]
+        private static partial Regex MyRegex();
+        [GeneratedRegex("(\\d{2})(\\d{5})(\\d{4})")]
+        private static partial Regex MyRegex1();
     }
 }
