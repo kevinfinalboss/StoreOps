af833a76eb9afefbcba8625924a868bef08271f0
Kevin Gomes <kevinmg50@gmail.com>
2023-08-09 10:51:50 -0300

    feat: nova opção de adicionar categoria implementada

--- src/UI/Menu.cs
+++ src/UI/Menu.cs
@@ -1,10 +1,19 @@
-using System;
 using Figgle;
+using StoreOps.Models;
+using StoreOps.Services;
+using System;
 
 namespace StoreOps
 {
     class Menu
     {
+        private readonly CategoryService _categoryService;
+
+        public Menu(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
         public void ShowMenu()
         {
             string asciiArt = FiggleFonts.Standard.Render("StoreOps");
@@ -16,13 +25,17 @@ namespace StoreOps
             {
                 Console.WriteLine("Menu Principal:");
                 Console.WriteLine("1 - Produtos");
-                Console.WriteLine("0 - Sair" );
+                Console.WriteLine("5 - Configuração");
+                Console.WriteLine("0 - Sair");
                 string option = Console.ReadLine();
 
                 switch (option)
                 {
                     case "1":
                         ShowProductMenu();
+                        break;
+                    case "5":
+                        ShowConfigurationMenu();
                         break;
                     case "0":
                         Environment.Exit(0);
@@ -34,7 +47,7 @@ namespace StoreOps
             }
         }
 
-        private void ShowProductMenu()
+        private static void ShowProductMenu()
         {
             while (true)
             {
@@ -62,6 +75,62 @@ namespace StoreOps
                         Console.WriteLine("Opção inválida!");
                         break;
                 }
+            }
+        }
+
+         private void ShowConfigurationMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("Menu de Configuração:");
+                Console.WriteLine("1 - Adicionar Categoria");
+                Console.WriteLine("2 - Editar Categoria");
+                Console.WriteLine("3 - Deletar Categoria");
+                Console.WriteLine("0 - Voltar ao Menu Principal");
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        AddCategory();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+            }
+        }
+        private void AddCategory()
+        {
+            try
+            {
+                Console.WriteLine("Informe o nome da categoria:");
+                string name = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(name)) throw new Exception("Nome da categoria não pode estar vazio.");
+
+                Console.WriteLine("Informe a descrição da categoria:");
+                string description = Console.ReadLine();
+
+                Category category = new()
+                {
+                    Id = null,
+                    Name = name,
+                    Description = description
+                };
+
+                _categoryService.AddCategory(category);
+
+                Console.WriteLine("Categoria adicionada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao adicionar categoria: {ex.Message}");
             }
         }
     }
