9641caed8f73ac681940db2c449f40183e2e2b26
Kevin Gomes <kevinmg50@gmail.com>
2023-08-09 09:56:05 -0300

    feat: modelo do produto

---
+++ src/Models/products.cs
@@ -0,0 +1,28 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace StoreOps.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public required string Name { get; set; }
+
+        [BsonElement("Description")]
+        public required string Description { get; set; }
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+
+        [BsonElement("CategoryId")]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string CategoryId { get; set; }
+
+        [BsonIgnore]
+        public required Category Category { get; set; }
+    }
+}
